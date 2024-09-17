21bf3b7b81885ced71a9ea1eeacdee7772b31e49
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-09-17 06:10:23 +0600

    design

---
+++ lib/components/stock.dart
@@ -0,0 +1,64 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame_example/components/waste.dart';
+import 'package:flame_example/klondike_game.dart';
+
+import 'card.dart';
+
+class StockPile extends PositionComponent with TapCallbacks {
+  @override
+  bool get debugMode => true;
+
+  StockPile({super.position}) : super(size: KlondikeGame.cardSize);
+
+  final List<Card> _cards = [];
+
+  void acquireCard(Card card) {
+    assert(!card.isFaceUp);
+    card.position = position;
+    card.priority = _cards.length;
+    _cards.add(card);
+  }
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    final wastePile = parent!.firstChild<WastePile>()!;
+
+    if (_cards.isEmpty) {
+      wastePile.removeAllCards().reversed.forEach((card) {
+        card.flip();
+        acquireCard(card);
+      });
+    } else {
+      for (int i = 0; i < 3; i++) {
+        if (_cards.isNotEmpty) {
+          final card = _cards.removeLast();
+          card.flip();
+          wastePile.acquireCard(card);
+        }
+      }
+    }
+  }
+
+  final _borderPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10
+    ..color = const Color(0xFF3F5B5D);
+
+  final _circlePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 100
+    ..color = const Color(0x883F5B5D);
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRRect(KlondikeGame.cardRRect, _borderPaint);
+    canvas.drawCircle(
+      Offset(width / 2, height / 2),
+      KlondikeGame.cardWidth * 0.3,
+      _circlePaint,
+    );
+  }
+}
---
+++ lib/components/tableau.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+
+import '../klondike_game.dart';
+import 'card.dart';
+
+class TableauPile extends PositionComponent {
+  @override
+  bool get debugMode => true;
+
+  TableauPile({super.position}) : super(size: KlondikeGame.cardSize);
+
+  final List<Card> _cards = [];
+  final Vector2 _fanOffset = Vector2(0, KlondikeGame.cardHeight * 0.05);
+
+  final _borderPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10
+    ..color = const Color(0x50ffffff);
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRRect(KlondikeGame.cardRRect, _borderPaint);
+  }
+
+  void acquireCard(Card card) {
+    if (_cards.isEmpty) {
+      card.position = position;
+    } else {
+      card.position = _cards.last.position + _fanOffset;
+    }
+    card.priority = _cards.length;
+    _cards.add(card);
+  }
+
+  void flipTopCard() {
+    assert(_cards.last.isFaceDown);
+    _cards.last.flip();
+  }
+}
